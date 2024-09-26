c9a848823e0e73d3904975782c8f4c3d22ca5faa
Jaimin Pandya <jaiminmoslake7020@gmail.com>
2024-09-25 22:25:23 -0700

    adding new Github Action Suppoort

--- apps/author.immigration-report.ca/pages/_document.tsx
+++ apps/author.immigration-report.ca/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import {inDevEnvironment} from '../src/utils/utils';
 
 export default function Document() {
@@ -10,13 +11,13 @@ export default function Document() {
                       integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
                       crossOrigin="anonymous" referrerPolicy="no-referrer"/>
                 {
-                    !inDevEnvironment && <script dangerouslySetInnerHTML={{
+                    !inDevEnvironment && <Script id={"gtm"} dangerouslySetInnerHTML={{
                         __html: `(function (w, d, s, l, i) {
                     w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
                     var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
                     j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
                 })(window, document, 'script', 'dataLayer', 'GTM-PLQDTLSG');`
-                    }}></script>
+                    }}></Script>
                 }
             </Head>
             <body>
